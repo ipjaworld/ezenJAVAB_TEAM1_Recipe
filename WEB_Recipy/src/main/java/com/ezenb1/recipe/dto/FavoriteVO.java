c5083c9a6daf5cd1383b62716c47f9c4f6a3fbee
Seungjun Ryu <seiko3156@kakao.com>
2022-12-28 15:52:07 +0900

    류승준 commit 20221228 1st

---
+++ WEB_Recipy/src/main/java/com/ezenb1/recipe/dto/FavoriteVO.java
@@ -0,0 +1,31 @@
+package com.ezenb1.recipe.dto;
+
+public class FavoriteVO {
+
+	private Integer fnum;
+	private String id;
+	private Integer rnum;
+	
+	
+	public Integer getFnum() {
+		return fnum;
+	}
+	public void setFnum(Integer fnum) {
+		this.fnum = fnum;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public Integer getRnum() {
+		return rnum;
+	}
+	public void setRnum(Integer rnum) {
+		this.rnum = rnum;
+	}
+	
+	
+	
+}
---
+++ WEB_Recipy/src/main/java/com/ezenb1/recipe/dto/InterestVO.java
@@ -0,0 +1,32 @@
+package com.ezenb1.recipe.dto;
+
+public class InterestVO {
+
+	private Integer interestnum;
+	private String id;
+	private Integer rnum;
+	
+	
+	public Integer getInterestnum() {
+		return interestnum;
+	}
+	public void setInterestnum(Integer interestnum) {
+		this.interestnum = interestnum;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public Integer getRnum() {
+		return rnum;
+	}
+	public void setRnum(Integer rnum) {
+		this.rnum = rnum;
+	}
+	
+	
+	
+	
+}
