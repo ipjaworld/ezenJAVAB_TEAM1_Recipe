fff0e7d0ead5106f8cc6152ed2361ae0beb478f0
Minjae Lee <ipjaworld@gmail.com>
2022-12-29 18:33:36 +0900

    건하 recipeFactory 수정

--- WEB_Recipy/src/main/java/com/ezenb1/recipe/controller/RecipeFactory.java
+++ WEB_Recipy/src/main/java/com/ezenb1/recipe/controller/RecipeFactory.java
@@ -10,7 +10,6 @@ import com.ezenb1.recipe.controller.action.admin.AdminMemListAction;
 import com.ezenb1.recipe.controller.action.admin.AdminQnaListAction;
 import com.ezenb1.recipe.controller.action.admin.AdminRecipeListAction;
 import com.ezenb1.recipe.controller.action.admin.AdminReplyListAction;
-import com.ezenb1.recipe.controller.action.admin.AdminSleepMemAction;
 
 public class RecipeFactory {
 
@@ -30,22 +29,33 @@ public class RecipeFactory {
 		else if( command.equals("adminDeleteMem") ) ac = new AdminDeleteMemAction();
 		else if( command.equals("adminDeleteReply") ) ac = new AdminDeleteReplyAction();
 		else if( command.equals("adminMemDetail") ) ac = new AdminMemDetailAction();
-		else if( command.equals("adminSleepMem") ) ac = new AdminSleepMemAction();
 		
+		else if( command.equals("findAccountForm") ) ac = new FindAccountFormAction();
+		else if( command.equals("findAccountBy") ) ac = new FindAccountByAction();
+		
+		else if( command.equals("joinForm") ) ac = new JoinFormAction();
+		else if( command.equals("join") ) ac = new JoinAction();
+		else if( command.equals("idCheckForm") ) ac = new IdCheckFormAction();
+		else if( command.equals("updateMemForm") ) ac = new UpdateMemFormAction();
+		else if( command.equals("updateMember") ) ac = new UpdateMemberAction();
+		else if( command.equals("withdrawal") ) ac = new WithdrawalAction();
+		
+		else if( command.equals("qnaList") ) ac = new QnaListAction();
+		else if( command.equals("myqnaList") ) ac = new MyqnaListAction();
+		else if( command.equals("qnaDetail") ) ac = new QnaDetailAction();
+		else if( command.equals("qnaWriteForm") ) ac = new QnaWriteFormAction();
+		else if( command.equals("writeQna") ) ac = new WriteQnaAction();
+		else if( command.equals("qnaUpdateForm") ) ac = new QnaUpdateFormAction();
+		else if( command.equals("updateQna") ) ac = new UpdateQnaAction();
+		else if( command.equals("deleteQna") ) ac = new DeleteQnaAction();
 		
 		/**
 		 * 여기 밑에서부터 붙여넣기 해주시면 됩니다.
 		 
-		else if( command.equals("joinForm") ) ac = new JoinFormAction();
-		else if( command.equals("join") ) ac = new JoinAction();
-		else if( command.equals("idCheckForm") ) ac = new IdCheckFormAction();
+		
 		else if( command.equals("loginForm") ) ac = new LoginFormAction();
 		else if( command.equals("login") ) ac = new LoginAction();
 		else if( command.equals("logout") ) ac = new LogoutAction();
-		
-		else if( command.equals("updateMemForm") ) ac = new UpdateMemFormAction();
-		else if( command.equals("updateMember") ) ac = new UpdateMemberAction();
-		else if( command.equals("withdrawal") ) ac = new WithdrawalAction();
 		
 		else if( command.equals("recipeForm") ) ac = new RecipeFormAction();
 		else if( command.equals("writeRecipe") ) ac = new WriteRecipeAction();
@@ -66,15 +76,7 @@ public class RecipeFactory {
 		else if( command.equals("myRecipeList") ) ac = new MyRecipeListAction();
 		else if( command.equals("recentView") ) ac = new RecentViewAction();
 		else if( command.equals("favoriteView") ) ac = new FavoriteViewAction();
-		
-		else if( command.equals("qnaList") ) ac = new QnaListAction();
-		else if( command.equals("myqnaList") ) ac = new MyqnaListAction();
-		else if( command.equals("qnaDetail") ) ac = new QnaDetailAction();
-		else if( command.equals("qnaWriteForm") ) ac = new QnaWriteFormAction();
-		else if( command.equals("writeQna") ) ac = new WriteQnaAction();
-		else if( command.equals("qnaUpdateForm") ) ac = new QnaUpdateFormAction();
-		else if( command.equals("updateQna") ) ac = new UpdateQnaAction();
-		else if( command.equals("deleteQna") ) ac = new DeleteQnaAction();
+
 		*/
 		
 		return ac;
